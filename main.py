fe68a4fc4969583ccab2c31ad88f94d7f4f7bcb3
Noyan Toksoy <n.e.toksoy@student.tudelft.nl>
2022-05-20 21:36:33 +0200

    fetch data

--- main.py
+++ main.py
@@ -1,41 +1,68 @@
-import json
-import requests
-from pprint import pprint
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import random
 
 PATH = "/Users/noyantoksoy/Downloads/chromedriver"
 
+global driver
+driver = webdriver.Chrome(PATH)
+
+
+def fetch_games(day, month, year):
+    URL = f"https://www.opendota.com/explorer?sql=SELECT%0Amatches.match_id%2C%0Amatches.start_time%2C%0Amatches.game_mode%2C%0Aplayer_matches.account_id%2C%0Aplayer_matches.hero_id%2C%0Aplayer_matches.kills%2C%0Aplayer_matches.deaths%2C%0Aplayer_matches.assists%2C%0Aplayer_matches.gold_per_min%2C%0Aplayer_matches.xp_per_min%2C%0Aplayer_matches.gold_spent%2C%0Aplayer_matches.hero_damage%2C%0Aplayer_matches.tower_damage%2C%0Aplayer_matches.hero_healing%2C%0Aplayer_matches.level%2C%0Amatches.dire_score%2C%0Amatches.draft_timings%2C%0Amatches.picks_bans%2C%0Amatches.objectives%2C%0Amatches.radiant_gold_adv%2C%0Amatches.radiant_score%2C%0Amatches.radiant_win%2C%0Amatches.radiant_xp_adv%2C%0Aplayer_matches.account_id%2C%0Aplayer_matches.lane_pos%2C%0Aplayer_matches.lane%2C%0Aplayer_matches.lane_role%2C%0Aplayer_matches.net_worth%0AFROM%20matches%0AJOIN%20match_patch%20using(match_id)%0ARIGHT%20JOIN%20player_matches%20using(match_id)%0AWHERE%20TRUE%0AAND%20matches.start_time%20%3E%3D%20extract(epoch%20from%20timestamp%20%27{year}-{month}-{day}T18%3A26%3A47.482Z%27)%0AAND%20matches.game_mode%20%3C%3D%202%0AAND%20player_matches.hero_id%20!%3D%2014%0AORDER%20BY%20matches.match_id%20NULLS%20LAST%0ALIMIT%203000&format="
+    driver.get(URL)
+    try:
+        results = WebDriverWait(driver, 15).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "sc-cJSrbW"))
+        )
+        csv_link = WebDriverWait(driver, 15).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "CSV"))
+        )
+        rows = driver.find_element(by=By.XPATH, value="/html/body/div/div/div[2]/div/div[4]/span[2]")
+        collected_games = int(rows.text.split(" ")[0])/10
+        print(collected_games)
+        setAttribute(csv_link, "download", f"data_300_{month}_{day}.csv")
+        csv_link.click()
+        time.sleep(0.5)
+        return collected_games
+    except Exception as e:
+        print(e)
+        return 0
+
+
+def setAttribute(element, att_name, att_value):
+    driver.execute_script("arguments[0].setAttribute(arguments[1], arguments[2]);",
+                          element, att_name, att_value)
+
+
 if __name__ == '__main__':
-    driver = webdriver.Chrome(PATH)
-    driver.get("https://www.opendota.com/explorer")
-
-    # response_API = requests.get('https://api.opendota.com/api/heroes')     # Omniknight
-    # # response_API = requests.get('https://api.opendota.com/api/heroes/14/matches')   # Pudge
-    # data = response_API.text
-    # parse_json = json.loads(data)
-    # pprint("")
-
-    # g = gr.Digraph(format='png')
-    #
-    # g.node("Selection of the Pudge hero")
-    # g.node("Game outcome")
-    #
-    # g.edge("Opponent’s hero pick", "Selection of the Pudge hero")
-    # g.edge("Opponent’s hero pick", "Game outcome")
-    # g.edge("Opponent’s hero pick", "Team building")
-    #
-    # g.edge("Team building", "Selection of the Pudge hero")
-    # g.edge("Team building", "Game outcome")
-    #
-    # g.edge("Knowledge", "Skill level")
-    # g.edge("Skill level", "MMR")
-    #
-    # g.edge("MMR", "Selection of the Pudge hero")
-    # g.edge("MMR", "Game outcome")
-    #
-    # g.edge("Strategy of the player", "Selection of the Pudge hero")
-    # g.edge("Strategy of the player", "Game outcome")
-    #
-    # g.edge("Past experience with the hero", "Selection of the Pudge hero")
-    # g.edge("Past experience with the hero", "Game outcome")
-    # g.view()
+    games_to_collect = 25000
+    games_collected = 0
+    day = 24
+    month = 3
+    year = 2022
+    while games_collected < games_to_collect:
+        text_month = ""
+        if month < 10:
+            text_month = "0" + str(month)
+        else:
+            text_month = "" + str(month)
+        collected = fetch_games(day, text_month, year)
+        games_collected += collected
+        new_day = day - random.randrange(start=1, stop=5)
+        if new_day > 0:
+            day = new_day
+        else:
+            day = 31
+            new_month = month - 1
+            if new_month > 0:
+                month = new_month
+            else:
+                month = 12
+                year = year - 1
+        time.sleep(2)
+    driver.quit()
