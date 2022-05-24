3482b0163879e1992f87824cbb716b31e44b85d4
Noyan Toksoy <n.e.toksoy@student.tudelft.nl>
2022-05-25 01:30:40 +0200

    new method to gather data

---
+++ gather_data_treatment.py
@@ -0,0 +1,43 @@
+import json
+import requests
+from pprint import pprint
+import csv
+import time
+from main import merge_csv
+
+if __name__ == '__main__':
+    # merge_csv()
+    base_path = "/Users/noyantoksoy/Downloads/gather_data_treatment_"
+    games_collected = 0
+    goal = 60000000
+    pudge_picked: list[dict] = []
+    match_id = 6583786900
+    while games_collected < goal:
+        try:
+            URL = f"https://api.opendota.com/api/publicMatches?less_than_match_id={match_id}"
+            time.sleep(0.5)
+            response_API = requests.get(URL)
+            data = response_API.text
+            parse_json = json.loads(data)
+            pudge_picked += list(
+                filter(lambda x: (x["game_mode"] in [1, 2, 22]) and x["avg_mmr"] is not None and (
+                        "14" in x["dire_team"].split(",") or "14" in x["radiant_team"].split(",")), parse_json))
+            match_id = match_id - 1000 if len(pudge_picked) == 0 else pudge_picked[-1]["match_id"]
+            games_collected += len(pudge_picked)
+            print(games_collected, "/", 60000000)
+        except Exception as e:
+            print(match_id)
+            match_id = match_id - 1000
+            continue
+
+    print(len(pudge_picked))
+    with open(base_path + f"/data_{len(pudge_picked)}_{match_id}.csv", "w", newline="") as f:
+        header = "match_id,match_seq_num,radiant_win,start_time,duration,avg_mmr,num_mmr,lobby_type,game_mode," \
+                 "avg_rank_tier," \
+                 "num_rank_tier,cluster,radiant_team,dire_team".split(",")
+        cw = csv.DictWriter(f, header)
+        cw.writeheader()
+        cw.writerows(pudge_picked)
+    pprint(len(pudge_picked))
+
+
--- main.py
+++ main.py
@@ -16,23 +16,24 @@ chromeOptions.add_experimental_option("prefs", prefs)
 PATH = "/Users/noyantoksoy/Downloads/chromedriver"
 
 global driver
-driver = webdriver.Chrome(executable_path=PATH, options=chromeOptions)
+# driver = webdriver.Chrome(executable_path=PATH, options=chromeOptions)
 
 
 def fetch_games(day, month, year, URL):
     driver.get(URL)
     try:
-        results = WebDriverWait(driver, 15).until(
+        results = WebDriverWait(driver, 8).until(
             EC.visibility_of_element_located((By.CLASS_NAME, "sc-cJSrbW"))
         )
-        csv_link = WebDriverWait(driver, 15).until(
+        csv_link = WebDriverWait(driver, 8).until(
             EC.element_to_be_clickable((By.LINK_TEXT, "CSV"))
         )
-        setAttribute(csv_link, "download", f"data_500_{month}_{day}_{year}.csv")
-        csv_link.click()
-        time.sleep(0.5)
         rows = driver.find_element(by=By.XPATH, value="/html/body/div/div/div[2]/div/div[4]/span[2]")
-        return int(rows.text.split(" ")[0])/10
+        collected_games = int(rows.text.split(" ")[0])/10
+        setAttribute(csv_link, "download", f"data_{collected_games}_{month}_{day}_{year}.csv")
+        time.sleep(0.5)
+        csv_link.click()
+        return collected_games
     except Exception as e:
         print(e)
         return 0
@@ -44,57 +45,16 @@ def setAttribute(element, att_name, att_value):
 
 
 def merge_csv():
-    files = os.path.join("/Users/noyantoksoy/Downloads/gather_data_treatment", "data*.csv")
+    files = os.path.join("/Users/noyantoksoy/Downloads/gather_data_treatment_", "data*.csv")
     files = glob.glob(files)
     df = pd.concat(map(pd.read_csv, files), ignore_index=True)
-    pre_process_csv(df, "/Users/noyantoksoy/Downloads/data_merged_treatment.csv")
-
-
-def pre_process_csv_treatment(df: pandas.DataFrame):
-    df.drop_duplicates(subset=["match_id", "start_time"],
-                       keep="first", inplace=True, ignore_index=True)
-    matches_found = 0
-    for match_id in df["match_id"].tolist():
-        URL = f"https://www.opendota.com/explorer?sql=SELECT%0Amatches.match_id%0AFROM%20matches%0ARIGHT%20JOIN%20player_matches%20using(match_id)%0AWHERE%20TRUE%0AAND%20matches.match_id%20%3D%20{match_id}%0AAND%20player_matches.hero_id%20%3D%2014%0AORDER%20BY%20matches.match_id%20NULLS%20LAST%0ALIMIT%20100&format="
-        driver.get(URL)
-        try:
-            results = WebDriverWait(driver, 5).until(
-                EC.visibility_of_element_located((By.CLASS_NAME, "sc-cJSrbW"))
-            )
-            csv_link = WebDriverWait(driver, 5).until(
-                EC.element_to_be_clickable((By.LINK_TEXT, "CSV"))
-            )
-            rows = driver.find_element(by=By.XPATH, value="/html/body/div/div/div[2]/div/div[4]/span[2]")
-            collect_match = True if int(rows.text.split(" ")[0]) == 1 else False
-            if collect_match:
-                time.sleep(0.5)
-                MATCH_URL = f"https://www.opendota.com/explorer?sql=SELECT%0Amatches.match_id%2C%0Amatches.start_time%2C%0Amatches.game_mode%2C%0Aplayer_matches.account_id%2C%0Aplayer_matches.hero_id%2C%0Aplayer_matches.kills%2C%0Aplayer_matches.deaths%2C%0Aplayer_matches.assists%2C%0Aplayer_matches.gold_per_min%2C%0Aplayer_matches.xp_per_min%2C%0Aplayer_matches.gold_spent%2C%0Aplayer_matches.hero_damage%2C%0Aplayer_matches.tower_damage%2C%0Aplayer_matches.hero_healing%2C%0Aplayer_matches.level%2C%0Amatches.dire_score%2C%0Amatches.draft_timings%2C%0Amatches.picks_bans%2C%0Amatches.objectives%2C%0Amatches.radiant_gold_adv%2C%0Amatches.radiant_score%2C%0Amatches.radiant_win%2C%0Amatches.radiant_xp_adv%2C%0Aplayer_matches.account_id%2C%0Aplayer_matches.lane_pos%2C%0Aplayer_matches.lane%2C%0Aplayer_matches.lane_role%2C%0Aplayer_matches.net_worth%0AFROM%20matches%0AJOIN%20match_patch%20using(match_id)%0ARIGHT%20JOIN%20player_matches%20using(match_id)%0AWHERE%20TRUE%0AAND%20matches.match_id%20%3D%20{match_id}%0AORDER%20BY%20matches.match_id%20NULLS%20LAST%0ALIMIT%20100&format="
-                driver.get(MATCH_URL)
-                try:
-                    results = WebDriverWait(driver, 5).until(
-                        EC.visibility_of_element_located((By.CLASS_NAME, "sc-cJSrbW"))
-                    )
-                    csv_link = WebDriverWait(driver, 5).until(
-                        EC.element_to_be_clickable((By.LINK_TEXT, "CSV"))
-                    )
-                    setAttribute(csv_link, "download", f"data_{match_id}_{month}_{day}_{year}.csv")
-                    time.sleep(0.5)
-                    csv_link.click()
-                    matches_found += 1
-                except Exception as e:
-                    print(e)
-                    continue
-            else:
-                continue
-        except Exception as e:
-            print(e)
-            continue
-
-    return matches_found
+    pre_process_csv(df, "/Users/noyantoksoy/Downloads/data_merged_treatment_new.csv")
 
 
 def pre_process_csv(df: pandas.DataFrame, file_path):
-    df.drop_duplicates(subset=["match_id", "account_id", "start_time"],
+    print(df.match_id.nunique())
+
+    df.drop_duplicates(subset=["match_id", "match_seq_num", "start_time"],
                        keep="first", inplace=True, ignore_index=True)
     # print(len(df.match_id.value_counts()[df.match_id.value_counts() < 10]))
     # for invalid in df.match_id.value_counts()[df.match_id.value_counts() < 10].index.values:
@@ -105,33 +65,33 @@ def pre_process_csv(df: pandas.DataFrame, file_path):
 
 
 if __name__ == '__main__':
-    games_to_collect = 25000
+    games_to_collect = 40000
     games_collected = 0
-    day = 21
+    day = 9
     month = 5
     year = 2022
-    # merge_csv()
-    while games_collected < games_to_collect:
-        text_month = ""
-        if month < 10:
-            text_month = "0" + str(month)
-        else:
-            text_month = "" + str(month)
-        URL_treatment = f"https://www.opendota.com/explorer?sql=SELECT%0Amatches.match_id%2C%0Amatches.start_time%2C%0Amatches.game_mode%2C%0Aplayer_matches.account_id%2C%0Aplayer_matches.hero_id%2C%0Aplayer_matches.kills%2C%0Aplayer_matches.deaths%2C%0Aplayer_matches.assists%2C%0Aplayer_matches.gold_per_min%2C%0Aplayer_matches.xp_per_min%2C%0Aplayer_matches.gold_spent%2C%0Aplayer_matches.hero_damage%2C%0Aplayer_matches.tower_damage%2C%0Aplayer_matches.hero_healing%2C%0Aplayer_matches.level%2C%0Amatches.dire_score%2C%0Amatches.draft_timings%2C%0Amatches.picks_bans%2C%0Amatches.objectives%2C%0Amatches.radiant_gold_adv%2C%0Amatches.radiant_score%2C%0Amatches.radiant_win%2C%0Amatches.radiant_xp_adv%2C%0Aplayer_matches.account_id%2C%0Aplayer_matches.lane_pos%2C%0Aplayer_matches.lane%2C%0Aplayer_matches.lane_role%2C%0Aplayer_matches.net_worth%0AFROM%20matches%0ARIGHT%20JOIN%20player_matches%20using(match_id)%0AWHERE%20match_id%20IN%20(%0A%20%20%20%20SELECT%0A%20%20%20%20matches.match_id%0A%20%20%20%20FROM%20matches%0A%20%20%20%20RIGHT%20JOIN%20player_matches%20using(match_id)%0A%20%20%20%20WHERE%20TRUE%0A%20%20%20%20AND%20matches.start_time%20%3E%3D%20extract(epoch%20from%20timestamp%20%27{year}-{month}-{day}T15%3A21%3A53.222Z%27)%0A%20%20%20%20AND%20player_matches.hero_id%20%3D%2014%0A%20%20%20%20AND%20matches.game_mode%20%3C%3D%202%0A%20%20%20%20ORDER%20BY%20matches.match_id%20NULLS%20LAST%0A%20%20%20%20LIMIT%202000%0A)%0AAND%20matches.start_time%20%3E%3D%20extract(epoch%20from%20timestamp%20%27{year}-{month}-{day}T18%3A26%3A47.482Z%27)%0AAND%20matches.game_mode%20%3C%3D%202%0AORDER%20BY%20matches.match_id%20NULLS%20LAST%0ALIMIT%206000&format="
-        collected = fetch_games(day, text_month, year, URL_treatment)
-        games_collected += collected
-        new_day = day - random.randrange(start=8, stop=20)
-        if new_day > 0:
-            day = new_day
-        else:
-            day = 31
-            new_month = month - 1
-            if new_month > 0:
-                if new_month == 2:
-                    day = 28
-                month = new_month
-            else:
-                month = 12
-                year = year - 1
-        time.sleep(2)
-    driver.quit()
+    merge_csv()
+    # while games_collected < games_to_collect:
+    #     text_month = ""
+    #     if month < 10:
+    #         text_month = "0" + str(month)
+    #     else:
+    #         text_month = "" + str(month)
+    #     URL_treatment = f"https://www.opendota.com/explorer?sql=SELECT%0Amatches.match_id%2C%0Amatches.start_time%2C%0Amatches.game_mode%2C%0Aplayer_matches.account_id%2C%0Aplayer_matches.hero_id%2C%0Aplayer_matches.kills%2C%0Aplayer_matches.deaths%2C%0Aplayer_matches.assists%2C%0Aplayer_matches.gold_per_min%2C%0Aplayer_matches.xp_per_min%2C%0Aplayer_matches.gold_spent%2C%0Aplayer_matches.hero_damage%2C%0Aplayer_matches.tower_damage%2C%0Aplayer_matches.hero_healing%2C%0Aplayer_matches.level%2C%0Amatches.dire_score%2C%0Amatches.radiant_score%2C%0Amatches.radiant_win%2C%0Aplayer_matches.lane%2C%0Aplayer_matches.lane_role%2C%0Aplayer_matches.net_worth%0AFROM%20matches%0ARIGHT%20JOIN%20player_matches%20using(match_id)%0AWHERE%20match_id%20IN%20(%0A%20%20%20%20SELECT%0A%20%20%20%20matches.match_id%0A%20%20%20%20FROM%20matches%0A%20%20%20%20RIGHT%20JOIN%20player_matches%20using(match_id)%0A%20%20%20%20WHERE%20TRUE%0A%20%20%20%20AND%20matches.start_time%20%3E%3D%20extract(epoch%20from%20timestamp%20%27{year}-{month}-{day}T18%3A26%3A47.482Z%27)%0A%20%20%20%20AND%20player_matches.hero_id%20%3D%2014%0A%20%20%20%20AND%20matches.game_mode%20%3C%3D%202%0A%20%20%20%20ORDER%20BY%20matches.match_id%20NULLS%20LAST%0A%20%20%20%20LIMIT%206000%0A)%0AAND%20matches.start_time%20%3E%3D%20extract(epoch%20from%20timestamp%20%27{year}-{month}-{day}T18%3A26%3A47.482Z%27)%0AAND%20matches.game_mode%20%3C%3D%202%0AORDER%20BY%20matches.match_id%20NULLS%20LAST%0ALIMIT%2030000&format="
+    #     collected = fetch_games(day, text_month, year, URL_treatment)
+    #     games_collected += collected
+    #     new_day = day - random.randrange(start=15, stop=25)
+    #     if new_day > 0:
+    #         day = new_day
+    #     else:
+    #         day = 31
+    #         new_month = month - 1
+    #         if new_month > 0:
+    #             if new_month == 2:
+    #                 day = 28
+    #             month = new_month
+    #         else:
+    #             month = 12
+    #             year = year - 1
+    #     time.sleep(2)
+    # driver.quit()
